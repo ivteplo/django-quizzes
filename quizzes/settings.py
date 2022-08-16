d709727199d7ed1e09e518e71ddc943e5a286a5b
ivteplo <iteplov7@gmail.com>
2022-08-16 23:04:01 +0300

    Start splitting one app to multiple

--- quizzes/settings.py
+++ quizzes/settings.py
@@ -38,7 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'authentication',
     'quizzes_app',
+    'app',
     'ui'
 ]
 
