7d4842bbe3ccc3d70f6a1457a098d7355c2536a0
ivteplo <iteplov7@gmail.com>
2022-08-17 12:55:28 +0300

    Move auth views to authentication folder

--- quizzes_app/urls.py
+++ quizzes_app/urls.py
@@ -7,11 +7,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
 
-    # auth
-    path('auth/register', views.register, name='register'),
-    path('auth/sign-in', views.sign_in, name='sign-in'),
-    path('auth/sign-out', views.sign_out, name='sign-out'),
-
     # quizzes
     path('quiz/new', views.new_quiz, name='new-quiz'),
     path('quiz/<str:quiz_url>', views.quiz_page, name='quiz-page')
